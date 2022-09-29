8b460fd8b70409b5b79b2bb4608d2d15e7eb2cef
Nabzozifo <nabzozifo@gmail.com>
2022-09-29 12:40:36 +0100

    second commit2

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -4,6 +4,8 @@ from .qtran_learner import QLearner as QTranLearner
 from .actor_critic_learner import ActorCriticLearner
 from .maddpg_learner import MADDPGLearner
 from .ppo_learner import PPOLearner
+from .ppo_learner_meta import PPOMetaLearner
+from .meta_actor_critic_learnear import MetaActorCriticLearner
 REGISTRY = {}
 
 REGISTRY["q_learner"] = QLearner
@@ -12,3 +14,5 @@ REGISTRY["qtran_learner"] = QTranLearner
 REGISTRY["actor_critic_learner"] = ActorCriticLearner
 REGISTRY["maddpg_learner"] = MADDPGLearner
 REGISTRY["ppo_learner"] = PPOLearner
+REGISTRY["ppo_learner_meta"] = PPOMetaLearner
+REGISTRY["actor_critic_learner_meta"] = MetaActorCriticLearner
